140a55ebffb46938fbe449686d513a1d04afad2b
Danico-mfala <mfaladan@hotmail.com>
2022-08-13 18:54:53 +0300

    complete

--- src/kata7.getEmployerRole.js
+++ src/kata7.getEmployerRole.js
@@ -1,3 +1,5 @@
-const getEmployerRole = (employeeName, employees) => {};
-
-module.exports = getEmployerRole;
+const getEmployerRole = (employeeName, employees) => {
+    let employee = employees.find((element) => element.name === employeeName);
+    return employee.role;
+  };
+  module.exports = getEmployerRole;
